af231f645519006de6e163e0f111b99d0a86c646
Minneh Mugo <minneh.mugo@gmail.com>
2017-03-02 10:06:38 +0300

    add form submit listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Back-end logic
 function Contact(first, last){
   this.firstName = first;
   this.lastName = last;
@@ -9,3 +10,24 @@ function Address(street, city, county){
   this.city = city;
   this.county = county;
 }
+
+
+//User Interface Logic
+$(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-county">County</label>' +
+                                   '<input type="text" class="form-control new-county">' +
+                                 '</div>' +
+                               '</div>');
+  });
+});
